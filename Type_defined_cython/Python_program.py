c89e009cccf6662f1025d909f32a04571fe5924d
k708 <k708abc@yahoo.co.jp>
2022-10-01 00:04:40 +0900

    Define type (middle of revision)

---
+++ Type_defined_cython/Python_program.py
@@ -0,0 +1,23 @@
+from main_function import functions
+
+
+class random_walk(functions):
+    def __init__(self):
+        functions.__init__(self)
+
+    def end(self):
+        print("Recording")
+        self.end_of_loop()
+        print("Finished: " + str(self.minute) + " min " + str(self.second) + " sec")
+
+    def start(self):
+        print("Calculation start")
+        self.start_setting()
+        print("Loop start")
+        self.loop()
+        self.end()
+
+
+if __name__ == "__main__":
+    rw_class = random_walk()
+    rw_class.start()
---
+++ Type_defined_cython/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+from Cython.Build import cythonize
+import Cython.Compiler.Options
+
+Cython.Compiler.Options.annotate = True
+
+setup(ext_modules=cythonize("sub_functions.pyx"), annotate=True)
+setup(ext_modules=cythonize("recording.pyx"), annotate=True)
+setup(ext_modules=cythonize("InputParameter.pyx"), annotate=True)
+setup(ext_modules=cythonize("main_function.pyx"), annotate=True)

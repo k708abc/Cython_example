97e3aaaed5c9017e1734180b180caa3eb801fd81
k708 <k708abc@yahoo.co.jp>
2022-10-01 00:03:22 +0900

    update

--- Use_cython/recording.pyx
+++ Use_cython/recording.pyx
@@ -8,5 +8,5 @@ def record_data(pos_rec, minute, second):
     plt.savefig("Record/heat_map.png")
     #
     file_data = open("Record/execution_time.txt", "w")
-    file_data.write(str(minute) + "min " + str(second) + "sec" "\n")
+    file_data.write(str(minute) + " min " + str(second) + " sec" "\n")
     file_data.close()
